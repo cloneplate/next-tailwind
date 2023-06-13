2bf929c52fb28b9b37a0de0170756eac96088a22
Randy <randypriv@gmail.com>
2023-06-13 13:26:13 +0800

    editor

--- packages/server/pages/index.tsx
+++ packages/server/pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Page () {
+import { Editor } from "../components/Editor/Editor";
+import { prisma } from '@monorepo/prisma'
+export default function Page() {
   return (
-    <div className="mx-auto container p-12">
-      Hello world
-    </div>
+    <div>
+
+    </div> 
   )
+}
+
+export const getServerSideProps = async (context) => {
+  // create new post
+  const post = await prisma.post.create({
+    data: {
+
+    }
+  }) 
+  
+  return {
+    redirect: {
+      
+    } 
+  }
 }
